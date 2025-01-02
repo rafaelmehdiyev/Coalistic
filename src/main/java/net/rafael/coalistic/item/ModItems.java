b5eb11f20f0434222bf855e23dd8134dfed85ed1
Rafael Mehdiyev <rafaelmehdiyev0@gmail.com>
2025-01-02 13:49:15 +0400

    Item Group Changed to Ingredients

--- src/main/java/net/rafael/coalistic/item/ModItems.java
+++ src/main/java/net/rafael/coalistic/item/ModItems.java
@@ -26,7 +26,7 @@ public class ModItems {
     public static void registerModItems() {
         Coalistic.LOGGER.info("Registering Mod Items for "+Coalistic.MOD_ID );
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(DOUBLE_COAL);
             entries.add(DOUBLE_CHARCOAL);
             entries.add(COAL_PIECE);
