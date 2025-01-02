f91f611d754a864ad7e7e18f1be1ce359c9a8fc7
Rafael Mehdiyev <rafaelmehdiyev0@gmail.com>
2025-01-02 13:38:21 +0400

    Fabric 1.21.4 Support

--- src/main/java/net/rafael/coalistic/datagen/ModRecipeProvider.java
+++ src/main/java/net/rafael/coalistic/datagen/ModRecipeProvider.java
@@ -1,10 +1,9 @@
 package net.rafael.coalistic.datagen;
 
-
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.RecipeGenerator;
+import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.recipe.RecipeGenerator;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryKey;
@@ -60,6 +59,6 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public String getName() {
-        return "";
+        return "Coalistic Recipes";
     }
 }
